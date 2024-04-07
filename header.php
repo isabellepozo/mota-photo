fa0ef365428438ff4446d0d13cb88b8a82ba9fb5
Isabelle Pozo <isabelle.pozo8@gmail.com>
2024-04-07 19:37:28 +0200

    Corrections responsive header

--- header.php
+++ header.php
@@ -9,13 +9,19 @@
 <body>
 <header class="header">   
     <div class="nav-header">
-        <img src="<?php echo esc_url(bloginfo('template_directory') . '/assets/images/logo.svg'); ?>" alt="<?php bloginfo('name'); ?>">
-        <div class="burger-menu"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/menu_burger_icon.png" alt="Menu Burger"></div>
-        <div class="close-menu hidden"><i class="fas fa-times"></i></div> <!-- Ajout de la balise pour la croix -->
+        <div class="logo">
+            <img src="<?php echo esc_url(bloginfo('template_directory') . '/assets/images/logo.svg'); ?>" alt="<?php bloginfo('name'); ?>">
+        </div>
+        <div class="burger-menu">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/menu_burger_icon.png" alt="Menu Burger">
+        </div>
+        <div class="close-menu hidden">
+            <i class="fas fa-times"></i>
+        </div> <!-- Ajout de la balise pour la croix -->
         <?php wp_nav_menu(array( 'theme_location' => 'menu-header' )) ?>
-        <h1><?php bloginfo("name") ?></h1>
     </div>
 </header>
+
 
 
 
